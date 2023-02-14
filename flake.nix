c21db67473a6e795099794ea97c872df271dea9b
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-02-14 14:18:51 -0500

    add test

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in {
+        devShells = {
+          default = pkgs.mkShell { buildInputs = with pkgs; [ nixfmt ]; };
+          CI = pkgs.mkShell {
+            buildInputs = with pkgs; [ hello ];
+            shellHook = ''
+              export TEST_VAR=TEST
+            '';
+          };
+        };
+      });
+}
